0c65fe6d8ab31a348ed9047874b00aca8ed13789
Scott L. McKenzie <fortyniners1234@gmail.com>
2026-08-22 22:50:47 -0500

    Added modules to top module. Need to work on top level logic.

---
+++ sources/axis_corner_turn_matrix.vhd
@@ -0,0 +1,90 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_misc.all;
+
+use work.ctm_package.all;
+
+entity axis_corner_turn_matrix is
+    generic (
+        C_NUMBER_ROWS : positive;
+        C_NUMBER_COLS : positive;
+        C_DATA_WIDTH  : integer range 8 to 1204
+    );
+    port (
+        AXIS_ACLK     : in  std_logic;
+        AXIS_ARSTN    : in  std_logic;
+        S_AXIS_TVALID : in  std_logic;
+        S_AXIS_TREADY : out std_logic;
+        S_AXIS_TDATA  : in  std_logic_vector(C_DATA_WIDTH - 1 downto 0);
+        M_AXIS_TVALID : out std_logic;
+        M_AXIS_TREADY : in  std_logic;
+        M_AXIS_TDATA  : out std_logic_vector(C_DATA_WIDTH - 1 downto 0)
+    );
+end entity axis_corner_turn_matrix;
+
+architecture synthesizable of axis_corner_turn_matrix is
+    constant C_BUFFER_SIZE    : integer := 256;
+    constant C_TOTAL_MEM_SIZE : integer := C_BUFFER_SIZE + 2 * C_NUMBER_COLS * C_NUMBER_ROWS;
+    constant C_ADDRESS_SIZE   : integer := clog2(C_TOTAL_MEM_SIZE);
+
+    signal tdpram_reset       : std_logic;
+    signal tdpram_input_en    : std_logic_vector(C_DATA_WIDTH / 8 - 1 downto 0);
+    signal input_count_en     : std_logic;
+    signal tdpram_input_addr  : std_logic_vector(C_ADDRESS_SIZE - 1 downto 0);
+
+    signal tdpram_output_en   : std_logic;
+    signal tdpram_output_addr : std_logic_vector(C_ADDRESS_SIZE - 1 downto 0);
+begin
+
+    tdpram_reset <= not AXIS_ARSTN;
+    input_count_en <= or_reduce(tdpram_input_en);
+
+    MEM : entity work.xilinx_tdpram_wrapper
+        generic map (
+            C_DATA_WIDTH   => C_DATA_WIDTH,
+            C_MEMORY_DEPTH => C_TOTAL_MEM_SIZE
+        )
+        port map (
+            CLK_A      => AXIS_ACLK,
+            RST_A      => tdpram_reset,
+            WRITE_EN_A => tdpram_input_en,
+            ADDRESS_A  => tdpram_input_addr,
+            DATA_IN_A  => S_AXIS_TDATA,
+            DATA_OUT_A => open,
+            RST_B      => tdpram_reset,
+            ENABLE_B   => tdpram_output_en,
+            ADDRESS_B  => tdpram_output_addr,
+            DATA_OUT_B => M_AXIS_TDATA
+        );
+
+    INPUT_CTRL : entity work.input_address_counter
+        generic map (
+            C_NUM_COLS       => C_NUMBER_ROWS * C_NUMBER_COLS,
+            C_ADDRESS_WIDTH  => C_ADDRESS_SIZE,
+            C_BASE_ADDRESS   => 0,
+            C_OFFSET_ADDRESS => C_TOTAL_MEM_SIZE / 2
+        )
+        port map (
+            CLK     => AXIS_ACLK,
+            RST     => tdpram_reset,
+            ENABLE  => input_count_en,
+            ADDRESS => unsigned(tdpram_input_addr)
+        );
+
+    OUTPUT_CTRL : entity work.output_address_counter
+        generic map (
+            C_NUM_ROWS       => C_NUMBER_ROWS,
+            C_NUM_COLS       => C_NUMBER_COLS,
+            C_ADDRESS_WIDTH  => C_ADDRESS_SIZE,
+            C_BASE_ADDRESS   => 0,
+            C_OFFSET_ADDRESS => C_TOTAL_MEM_SIZE / 2
+        )
+        port map (
+            CLK     => AXIS_ACLK,
+            RST     => tdpram_reset,
+            ENABLE  => tdpram_output_en,
+            ADDRESS => tdpram_input_addr
+        );
+
+end architecture synthesizable;
